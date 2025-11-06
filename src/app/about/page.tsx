01a4622709dc1213ab04c2e619e7a5f01241b17e
StenixDev <stenix.dev@gmail.com>
2025-11-06 16:11:56 +0800

    removed  about  and contact

--- src/app/about/page.tsx
+++
@@ -1,4 +0,0 @@
-const About = () => {
-  return <div>This is a about page</div>;
-};
-export default About;
--- src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-const Contact = () => {
-  return (
-    <div>
-      <h1 className='text-3xl'>Contact Page</h1>
-    </div>
-  );
-};
-export default Contact;
