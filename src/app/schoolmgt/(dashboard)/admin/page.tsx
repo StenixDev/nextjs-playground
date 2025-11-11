667915338244b3cfa9a25507008e07b5133b2844
StenixDev <stenix.dev@gmail.com>
2025-11-11 15:31:21 +0800

    student Chart

--- src/app/schoolmgt/(dashboard)/admin/page.tsx
+++ src/app/schoolmgt/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from '../../components/CountChart';
 import UserCard from '../../components/UserCard';
 
 function AdminPage() {
@@ -5,7 +6,7 @@ function AdminPage() {
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/* LEFT */}
 
-      <div className='w-full lg:w-2/3'>
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
         {/* USER CARD */}
 
         <div className='flex gap-4 justify-between flex-wrap'>
@@ -14,6 +15,19 @@ function AdminPage() {
           <UserCard type='parent' />
           <UserCard type='staff' />
         </div>
+        {/* Middle Charts */}
+        <div className='flex gap-4 flex-col lg:flex-row'>
+          {/* Count chart */}
+          <div className='w-full lg:w-1/3 h-[450px]'>
+            <CountChart />
+          </div>
+
+          {/* Attendance chart */}
+          <div className='w-full lg:w-2/3 h-[450px]'></div>
+        </div>
+
+        {/* Bottom Charts */}
+        <div className=''></div>
       </div>
 
       {/* RIGHT */}
