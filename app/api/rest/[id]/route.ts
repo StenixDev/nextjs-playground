9624e8758c8112e483861ac2f7e0d23a72c2540a
StenixDev <stenix.dev@gmail.com>
2026-02-25 12:19:21 +0800

    [exp] - REST API PUT

--- app/api/rest/[id]/route.ts
+++ app/api/rest/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { users } from '../route';
+import { success } from 'zod';
 
 export async function GET(request, { params }) {
   try {
@@ -7,13 +8,50 @@ export async function GET(request, { params }) {
     const userId = parseInt(id);
 
     const user = users.find((user) => user.id === userId);
+
     return NextResponse.json({
-      success: true,
+      success: user ? true : false,
       data: user,
     });
   } catch (error) {
     return NextResponse.json({
       error: "can't get user " + error,
+    });
+  }
+}
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = await params;
+    const userId = parseInt(id);
+    const { name, email } = await request.json();
+
+    const userIndex = users.findIndex((user) => user.id === userId);
+
+    if (userIndex === -1) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'user id not found',
+        },
+        { status: 404 },
+      );
+    }
+
+    users[userIndex] = {
+      id: userId,
+      email,
+      name,
+    };
+
+    return NextResponse.json({
+      success: true,
+      data: users[userIndex],
+      message: 'user successfully updated',
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: "can't update the user " + error,
     });
   }
 }
