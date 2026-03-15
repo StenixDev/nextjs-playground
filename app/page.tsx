c0ff362eabb5dfeb242d21f35946fdbf9e963912
StenixDev <stenix.dev@gmail.com>
2026-03-15 17:57:02 +0800

    [tanstack] - install

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,4 @@
-type UserType = {
-  id: number;
-  name: string;
-};
-
-async function Home() {
-  const res = await fetch('http://localhost:3000/api/rest');
-  const data = await res.json();
-
-  console.log(data);
-  return (
-    <div>
-      {data.users.map((user: UserType) => (
-        <span key={user.id} className='block'>
-          {user.name}
-        </span>
-      ))}
-    </div>
-  );
+function Home() {
+  return <div>Hello Tanstact Query</div>;
 }
 export default Home;
---
+++ components/query-provider.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+function QueryProvider({ children }: Props) {
+  const [client] = useState(() => new QueryClient());
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
+export default QueryProvider;
